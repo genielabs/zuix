970c77d4fa3efed1ba4d38d5925bf8725af27af6
Generoso Martello <generoso.martello@gmail.com>
2018-06-01 22:19:58 +0200

    ignoring output folder 'docs'

--- docs/config.js
+++
@@ -1,6 +0,0 @@
-zuix.store("config", {
-    "title": "zUIx Web Starter application.",
-    "googleSiteId": "UA-123-456",
-    "resourcePath": "/app/",
-    "libraryPath": "https://genielabs.github.io/zkit/lib"
-});
