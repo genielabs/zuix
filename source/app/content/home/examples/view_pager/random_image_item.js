0f8b6b4f7ad1eb1635bb74cc80505219fff7593b
Generoso Martello <generoso.martello@gmail.com>
2018-08-24 13:31:12 +0200

    fix view_pager example image drag issues with FireFox

--- source/app/content/home/examples/view_pager/random_image_item.js
+++ source/app/content/home/examples/view_pager/random_image_item.js
@@ -1,11 +1,5 @@
 zuix.controller(function(cp) {
     cp.create = function() {
-        cp.field('image')
-            .on('load', function() {
-                this.show();
-            })
-            .hide()
-            //.attr('src', 'http://lorempixel.com/420/280/?random&ts='+(new Date().getTime()));
-            .attr('src', 'https://picsum.photos/352/288/?random&ts='+(new Date().getTime()));
+        cp.view('.backdrop').css('background-image', 'url("https://picsum.photos/352/288/?random&ts='+(new Date().getTime())+'")');
     };
 });
